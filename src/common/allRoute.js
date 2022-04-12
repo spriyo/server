288596a8621f0958ee96e52f025c30f47b19a995
leostelon <53221136+leostelon@users.noreply.github.com>
2022-04-12 14:56:04 +0530

    removed waitlist

--- src/common/allRoute.js
+++ src/common/allRoute.js
@@ -1,8 +1,6 @@
 const router = require("express").Router();
 const nonce = require("./nonce");
-const waitlist = require("./waitlist");
 
 router.use(nonce);
-router.use(waitlist);
 
 module.exports = { commonRouter: router };
