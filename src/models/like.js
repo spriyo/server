f7f18bed1f6b828852e5d60eec0c32fb675dc2fb
leostelon <53221136+leostelon@users.noreply.github.com>
2022-06-09 09:16:20 +0530

    like model and function

---
+++ src/models/like.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const LikeSchema = new mongoose.Schema(
+	{
+		asset_id: {
+			type: mongoose.Types.ObjectId,
+			ref: "Asset",
+			required: true,
+		},
+		user_id: {
+			type: mongoose.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+	},
+	{ timestamps: true }
+);
+
+const Like = new mongoose.model("Like", LikeSchema);
+
+module.exports = { Like };
