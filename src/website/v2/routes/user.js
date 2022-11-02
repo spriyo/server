5cbb66489f4b05c2a2d0aecc53225de915519685
leostelon <53221136+leostelon@users.noreply.github.com>
2022-11-02 23:58:02 +0530

    user nfts

---
+++ src/website/v2/routes/user.js
@@ -0,0 +1,56 @@
+const { NFT } = require("../../../models/nft");
+const { Owner } = require("../../../models/owner");
+
+const router = require("express").Router();
+
+router.get("/users/nfts/:address", async (req, res) => {
+	try {
+		const owner = req.params.address;
+
+		let ownerQuery = {};
+		if (owner) {
+			ownerQuery["address"] = new RegExp(owner, "i");
+		}
+
+		const nfts = await Owner.aggregate([
+			{
+				$match: {
+					...ownerQuery,
+				},
+			},
+			{
+				$lookup: {
+					from: "nfts",
+					as: "nft",
+					let: {
+						contract_address: "$contract_address",
+						token_id: "$token_id",
+						chain_id: "$chain_id",
+					},
+					pipeline: [
+						{
+							$match: {
+								$and: [
+									{ $expr: { $eq: ["$contract_address", "$$contract_address"] } },
+									{ $expr: { $eq: ["$token_id", "$$token_id"] } },
+									{ $expr: { $eq: ["$chain_id", "$$chain_id"] } },
+								],
+							},
+						},
+					],
+				},
+			},
+            {
+				$unwind: { path: "$nft", preserveNullAndEmptyArrays: true },
+			},
+			{ $skip: parseInt(!req.query.skip ? 0 : req.query.skip) },
+			{ $limit: parseInt(!req.query.limit ? 10 : req.query.limit) },
+		]);
+
+		res.send(nfts);
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+module.exports = router;
