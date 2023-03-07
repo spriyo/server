f412870a0ceb357ab6fdd3ae8f27bf11535a1443
Leo Stelon <53221136+leostelon@users.noreply.github.com>
2023-03-07 15:21:41 +0530

    one day mint getstatistics bug

--- src/website/v2/controllers/display.js
+++ src/website/v2/controllers/display.js
@@ -51,8 +51,7 @@ const getStatistics = async function (req, res) {
 								"/" +
 								(new Date().getMonth() + 1) +
 								"/" +
-								new Date().getDate() +
-								1
+								new Date().getDate()
 						),
 					},
 				},
